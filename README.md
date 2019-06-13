7657893a9a72744e52cd3af7d898b8679974afd1
Mark Foster <foster154@gmail.com>
2019-06-13 08:48:36 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# The Coding Interview Bootcamp
+
+Companion repo to [The Coding Interview Bootcamp](https://www.udemy.com/coding-interview-bootcamp-algorithms-and-data-structure) on Udemy.
---
+++ exercises/reversestring/index.js
@@ -0,0 +1,18 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  debugger;
+  return str.split('').reduce((result, char) => {
+    return char + result
+  }, '')
+}
+
+reverse("Mark");
+
+module.exports = reverse;
